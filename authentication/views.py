3d21091c8f440e2e80c33f78567959b576e38d33
t3chstop <84115793+t3chstop@users.noreply.github.com>
2023-01-05 13:31:30 -0800

    send google response directly to authentication server and validate CSRF token

--- authentication/views.py
+++ authentication/views.py
@@ -14,35 +14,33 @@ from django.views.decorators.csrf import csrf_exempt
 #View to handle authentication through google
 @csrf_exempt
 def g_authenticate(request):	
-	
 	if (request.method=='GET'):
 		return HttpResponse("This page is meant to consume credentials...")
 
 	if (request.method=='POST'):
-		print(str(request.headers))
 
-		#Get JWT
-		token = request.body
+		#Validate CSRF Token
+		csrf_token_cookie = request.COOKIES.get('g_csrf_token')
+		if not csrf_token_cookie:
+			return HttpResponse("ERR_400: No CSRF Token in Cookie")
+		csrf_token_body = request.POST.__getitem__('g_csrf_token')
+		if not csrf_token_body:
+			return HttpResponse("ERR_400: No CSRF Token in POST body")
+		if csrf_token_cookie != csrf_token_body:
+			return HttpResponse("ERR_400: Failed to verify double submit cookie")
 
+		#Validate ID Token
+		token = request.POST.__getitem__("credential")
 		try:
-    		# Specify the CLIENT_ID of the app that accesses the backend:
 			idinfo = id_token.verify_oauth2_token(token, requests.Request(), "1089272278877-rhnl6uenbnsk1gkuhu3jb7kedn2suto5.apps.googleusercontent.com")
-
-			# Or, if multiple clients access the backend server:
-			# idinfo = id_token.verify_oauth2_token(token, requests.Request())
-			# if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
-			#     raise ValueError('Could not verify audience.')
-
-			# If auth request is from a G Suite domain:
-			# if idinfo['hd'] != GSUITE_DOMAIN_NAME:
-			#     raise ValueError('Wrong hosted domain.')
-
-			# ID token is valid. Get the user's Google Account ID from the decoded token.
 			userid = idinfo['sub']
 		except ValueError:
 			# Invalid token
 			return HttpResponse("Invalid JSON Web Token")
 
+
+
+		return HttpResponse("It worked")
 		# sessionid = get_random_string()
 		# newsession = CustomSession(sessionid=sessionid)
 		# newsession.expiry = datetime.now() + datetime.timedelta(days=0.5)
